55a861338a832ddc166a3aec76a5aee6adbaffc7
Daniel Vieites <dvieitest@gmail.com>
2026-04-20 21:24:45 +0200

    chore: check minimum neovim version

--- lua/minifugit/health.lua
+++ lua/minifugit/health.lua
@@ -1,17 +1,26 @@
 local M = {}
 
 local check_git_installed = function()
-    return vim.fn.executable("git")
+    if vim.fn.executable("git") then
+        vim.health.ok('`git` is installed')
+    else
+        vim.health.error('`git` is not installed')
+    end
+end
+
+local check_nvim_version = function()
+    if vim.version.ge(vim.version(), "0.10") then
+        vim.health.ok("Version v0.10+")
+    else
+        vim.health.error("Version should be v0.10+")
+    end
 end
 
 M.check = function()
     vim.health.start('minifugit.nvim report')
     -- make sure setup function parameters are ok
-    if check_git_installed() then
-        vim.health.ok('`git` is installed')
-    else
-        vim.health.error('`git` is not installed')
-    end
+    check_nvim_version()
+    check_git_installed()
 end
 
 return M
