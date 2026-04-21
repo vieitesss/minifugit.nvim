e4555946183c5375a26af7c336d43e264fe7e151
Daniel Vieites <dvieitest@gmail.com>
2026-04-21 22:37:43 +0200

    refactor: get_current_line function and new diff action

--- lua/minifugit/git_status/actions.lua
+++ lua/minifugit/git_status/actions.lua
@@ -3,29 +3,41 @@ local log = require('minifugit.log')
 
 ---@class GitStatusActions
 ---@field go_to_file function
+---@field diff function
 
 ---@type GitStatusActions
 local gsa = {
     go_to_file = function() end,
+    diff = function() end,
 }
 
-function gsa.go_to_file()
-    local status_win = vim.api.nvim_get_current_win()
+---@param status_win integer
+---@return MiniFugitLine?
+local function get_current_line(status_win)
     local pos = vim.api.nvim_win_get_cursor(status_win)
     local row = pos[1]
     local line = ui.get_line(row)
 
+    log.debug("line=" .. line.text)
+    return line
+end
+
+function gsa.go_to_file()
+    local status_win = vim.api.nvim_get_current_win()
+    local line = get_current_line(status_win)
+
     if line == nil or line.data == nil then
         return
     end
-
-    log.debug("line=" .. line.text)
 
     local file = vim.fn.fnamemodify(line.data.path, ':p')
 
     ui.focus_edit_target(status_win)
 
     vim.cmd('edit ' .. vim.fn.fnameescape(file))
+end
+
+function gsa.diff()
 
 end
 
