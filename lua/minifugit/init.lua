2c35a7e615a48c2025ff7107883fc8b514185169
Daniel Vieites <dvieitest@gmail.com>
2026-04-26 19:00:13 +0200

    feat: add minifugit commands

--- lua/minifugit/init.lua
+++ lua/minifugit/init.lua
@@ -1,7 +1,9 @@
 ---@class Minifugit
 ---@field gsw GitStatusWindow?
+---@field did_setup boolean
 local M = {
     gsw = nil,
+    did_setup = false,
 }
 
 local log = require('minifugit.log')
@@ -21,6 +23,28 @@ function M.status()
     log.info(
         string.format('Window opened win=%d buf=%d', M.gsw.win, M.gsw.buf.id)
     )
+end
+
+function M.setup()
+    if M.did_setup then
+        return
+    end
+
+    M.did_setup = true
+
+    vim.api.nvim_create_user_command('Minifugit', function()
+        M.status()
+    end, {
+        desc = 'Open Minifugit status',
+        force = true,
+    })
+
+    vim.api.nvim_create_user_command('MinifugitStatus', function()
+        M.status()
+    end, {
+        desc = 'Open Minifugit status',
+        force = true,
+    })
 end
 
 return M
